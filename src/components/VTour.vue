2c5c366cbbab0836dd1e05d39d2c930f525f8616
Tusko <tusko@photoinside.me>
2025-06-17 17:10:37 +0300

    fix: #89 updatePosition in window resize and resizeEnd

--- src/components/VTour.vue
+++ src/components/VTour.vue
@@ -1,11 +1,20 @@
 <script setup lang="ts">
 import { createPopper, type NanoPop } from "nanopop";
-import { computed, type ComputedRef, onMounted, type Reactive, reactive, type Ref, ref } from "vue";
+import {
+  computed,
+  type ComputedRef,
+  onMounted,
+  type Reactive,
+  reactive,
+  type Ref,
+  ref,
+  onUnmounted
+} from "vue";
 import type { ITourStep } from "../Types.ts";
 import jump from "jump.js";
 
 export interface IVTourProps {
-  name?: string,
+  name?: string;
   steps: ITourStep[];
   backdrop?: boolean;
   autoStart?: boolean;
@@ -18,10 +27,11 @@ export interface IVTourProps {
     done: string;
     skip: string;
   };
-  saveToLocalStorage?: 'never' | 'step' | 'end';
+  saveToLocalStorage?: "never" | "step" | "end";
   hideSkip?: boolean;
   hideArrow?: boolean;
   noScroll?: boolean;
+  resizeTimeout?: number;
 }
 export interface IVTourData {
   currentStep: number;
@@ -44,10 +54,10 @@ const _CurrentStep: Reactive<IVTourData> = reactive({
 });
 const props = defineProps<IVTourProps>();
 const emit = defineEmits<{
-  onTourStart: [],
-  onTourEnd: [],
-  onTourStep: []
-}>()
+  onTourStart: [];
+  onTourEnd: [];
+  onTourStep: [];
+}>();
 defineExpose({
   startTour,
   nextStep,
@@ -55,33 +65,50 @@ defineExpose({
   endTour,
   stopTour,
   goToStep,
-  resetTour
+  resetTour,
+
+  updatePosition,
+  updateHighlight,
+  updateBackdrop,
 });
 const getNextLabel: ComputedRef<String> = computed(() => {
-  if (_CurrentStep.currentStep === props.steps.length - 1) return props.buttonLabels?.done || 'Done';
-  return props.buttonLabels?.next || 'Next';
+  if (_CurrentStep.currentStep === props.steps.length - 1)
+    return props.buttonLabels?.done || "Done";
+  return props.buttonLabels?.next || "Next";
 });
 
-const getClipPath = ref(getClipPathValues('.vjt-highlight') ? getClipPathValues('.vjt-highlight') : '');
+const getClipPath = ref(
+  getClipPathValues(".vjt-highlight") ? getClipPathValues(".vjt-highlight") : ""
+);
 
 function startTour(): void {
-  if (localStorage.getItem('vjt-' + (props.name || 'default')) === 'true') return;
-  if (props.saveToLocalStorage === 'step') {
-    _CurrentStep.currentStep = parseInt(localStorage.getItem('vjt-' + (props.name || 'default')) || '0');
+  if (localStorage.getItem("vjt-" + (props.name || "default")) === "true")
+    return;
+  if (props.saveToLocalStorage === "step") {
+    _CurrentStep.currentStep = parseInt(
+      localStorage.getItem("vjt-" + (props.name || "default")) || "0"
+    );
     if (_CurrentStep.currentStep > 0) {
       _CurrentStep.lastStep = Math.max(_CurrentStep.currentStep - 1, 0);
       _CurrentStep.nextStep = _CurrentStep.currentStep + 1;
     }
-  }
-  else _CurrentStep.currentStep = 0;
+  } else _CurrentStep.currentStep = 0;
 
   setTimeout(async () => {
     await beforeStep(_CurrentStep.currentStep);
     if (!_VTour.value) {
-      _VTour.value = createPopper(document.querySelector(`${_CurrentStep.getCurrentStep.target}`) as HTMLElement, _Tooltip.value!, {
-        position: _CurrentStep.getCurrentStep.placement || 'right',
-        margin: props.margin || ((props.highlight || _CurrentStep.getCurrentStep.highlight) ? 14 : 8),
-      });
+      _VTour.value = createPopper(
+        document.querySelector(
+          `${_CurrentStep.getCurrentStep.target}`
+        ) as HTMLElement,
+        _Tooltip.value!,
+        {
+          position: _CurrentStep.getCurrentStep.placement || "right",
+          margin:
+            props.margin ||
+            (props.highlight || _CurrentStep.getCurrentStep.highlight ? 14 : 8),
+        }
+      );
     }
     updatePosition();
     emit("onTourStart");
@@ -89,9 +116,11 @@ function startTour(): void {
 }
 
 function stopTour(): void {
-  document.querySelector('#vjt-backdrop')!.setAttribute('data-hidden', '');
-  document.querySelectorAll('.vjt-highlight')!.forEach((element) => element.classList.remove('vjt-highlight'));
-  _Tooltip.value!.setAttribute('data-hidden', '');
+  document.querySelector("#vjt-backdrop")!.setAttribute("data-hidden", "");
+  document
+    .querySelectorAll(".vjt-highlight")!
+    .forEach((element) => element.classList.remove("vjt-highlight"));
+  _Tooltip.value!.setAttribute("data-hidden", "");
 }
 
 function resetTour(restart: boolean): void {
@@ -99,7 +128,7 @@ function resetTour(restart: boolean): void {
   _CurrentStep.currentStep = 0;
   _CurrentStep.lastStep = 0;
   _CurrentStep.nextStep = 1;
-  localStorage.removeItem('vjt-' + (props.name || 'default'));
+  localStorage.removeItem("vjt-" + (props.name || "default"));
   if (restart) startTour();
 }
 
@@ -132,7 +161,8 @@ async function lastStep() {
 
 function endTour(): void {
   stopTour();
-  if (props.saveToLocalStorage !== 'never') localStorage.setItem('vjt-' + (props.name || 'default'), 'true');
+  if (props.saveToLocalStorage !== "never")
+    localStorage.setItem("vjt-" + (props.name || "default"), "true");
   emit("onTourEnd");
 }
 
@@ -152,45 +182,87 @@ async function updatePosition(): Promise<void> {
   await new Promise<void>((resolve) => {
     updateHighlight();
     updateBackdrop();
-    _Tooltip.value!.setAttribute('data-hidden', '');
+    _Tooltip.value!.setAttribute("data-hidden", "");
     if (!props.noScroll && !_CurrentStep.getCurrentStep.noScroll) {
-      jump(document.querySelector(`${_CurrentStep.getCurrentStep.target}`) as HTMLElement, {
-        duration: 500,
-        offset: -100,
-        callback: () => { resolve() },
-      });
+      jump(
+        document.querySelector(
+          `${_CurrentStep.getCurrentStep.target}`
+        ) as HTMLElement,
+        {
+          duration: 500,
+          offset: -100,
+          callback: () => {
+            resolve();
+          },
+        }
+      );
     } else resolve();
   });
-  _Tooltip.value!.removeAttribute('data-hidden');
-  _Tooltip.value!.setAttribute('data-arrow', _VTour.value!.update({
-    reference: document.querySelector(`${_CurrentStep.getCurrentStep.target}`) as HTMLElement,
-    position: _CurrentStep.getCurrentStep.placement || 'right',
-  }) || 'right');
-  if (props.saveToLocalStorage === 'step') localStorage.setItem('vjt-' + (props.name || 'default'), _CurrentStep.currentStep.toString());
+  _Tooltip.value!.removeAttribute("data-hidden");
+  _Tooltip.value!.setAttribute(
+    "data-arrow",
+    _VTour.value!.update({
+      reference: document.querySelector(
+        `${_CurrentStep.getCurrentStep.target}`
+      ) as HTMLElement,
+      position: _CurrentStep.getCurrentStep.placement || "right",
+    }) || "right"
+  );
+  if (props.saveToLocalStorage === "step")
+    localStorage.setItem(
+      "vjt-" + (props.name || "default"),
+      _CurrentStep.currentStep.toString()
+    );
   await _CurrentStep.getCurrentStep.onAfter?.();
   emit("onTourStep");
 }
 
 function updateHighlight(): void {
-  document.querySelectorAll('.vjt-highlight').forEach((element) => element.classList.remove('vjt-highlight'));
+  document
+    .querySelectorAll(".vjt-highlight")
+    .forEach((element) => element.classList.remove("vjt-highlight"));
   if (!props.highlight && !_CurrentStep.getCurrentStep.highlight) return;
-  (document.querySelector(`${_CurrentStep.getCurrentStep.target}`) as HTMLElement).classList.add('vjt-highlight');
-  getClipPath.value = getClipPathValues('.vjt-highlight');
+  (
+    document.querySelector(
+      `${_CurrentStep.getCurrentStep.target}`
+    ) as HTMLElement
+  ).classList.add("vjt-highlight");
+  getClipPath.value = getClipPathValues(".vjt-highlight");
 }
 
 function updateBackdrop(): void {
-  if (props.backdrop || _CurrentStep.getCurrentStep.backdrop) document.querySelector('#vjt-backdrop')!.removeAttribute('data-hidden');
-  else document.querySelector('#vjt-backdrop')!.setAttribute('data-hidden', '');
+  if (props.backdrop || _CurrentStep.getCurrentStep.backdrop)
+    document.querySelector("#vjt-backdrop")!.removeAttribute("data-hidden");
+  else document.querySelector("#vjt-backdrop")!.setAttribute("data-hidden", "");
+}
+
+let resizeTimer: NodeJS.Timeout;
+const debounceTime = computed(() => props.resizeTimeout || 250);
+
+const onResizeEnd = () => {
+  updatePosition();
+
+  clearTimeout(resizeTimer);
+
+  resizeTimer = setTimeout(() => {
+      updatePosition();
+  }, debounceTime.value);
 }
 
 onMounted(() => {
-  _Tooltip.value = document.querySelector('#vjt-tooltip') as HTMLElement;
+  _Tooltip.value = document.querySelector("#vjt-tooltip") as HTMLElement;
   if (props.autoStart) startTour();
+
+  window.addEventListener("resize", onResizeEnd);
+});
+
+onUnmounted(() => {
+  window.removeEventListener("resize", onResizeEnd);
 });
 
 function getClipPathValues(targetSelector: string): string {
   const targetElement = document.querySelector(targetSelector) as HTMLElement;
-  if (!targetElement) return '';
+  if (!targetElement) return "";
 
   const rect = targetElement.getBoundingClientRect();
   return `polygon(
@@ -206,7 +278,6 @@ function getClipPathValues(targetSelector: string): string {
     100% 0%
   )`;
 }
-
 </script>
 
 <template>
@@ -217,10 +288,8 @@ function getClipPathValues(targetSelector: string): string {
     </slot>
     <slot name="actions" v-bind="{ lastStep, nextStep, endTour, _CurrentStep, getNextLabel, props }">
       <div class="vjt-actions">
-        <button v-if="_CurrentStep.lastStep < _CurrentStep.currentStep" type="button" @click.prevent="lastStep()"
-          v-text="props.buttonLabels?.back || 'Back'"></button>
-        <button v-if="!props.hideSkip" type="button" @click.prevent="endTour()"
-          v-text="props.buttonLabels?.skip || 'Skip'"></button>
+        <button v-if="_CurrentStep.lastStep < _CurrentStep.currentStep" type="button" @click.prevent="lastStep()" v-text="props.buttonLabels?.back || 'Back'"></button>
+        <button v-if="!props.hideSkip" type="button" @click.prevent="endTour()" v-text="props.buttonLabels?.skip || 'Skip'"></button>
         <button type="button" @click.prevent="nextStep()" v-text="getNextLabel"></button>
       </div>
     </slot>
